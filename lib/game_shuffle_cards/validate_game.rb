6c7e8c2c0559f596e2ddd2dbf45c6fab657eaf3a
Diego <diego@guiasrails.es>
2015-04-02 16:41:18 +0200

    ended version 1.0.0

--- lib/game_shuffle_cards/validate_game.rb
+++ lib/game_shuffle_cards/validate_game.rb
@@ -1,7 +1,14 @@
 
 module GameShuffleCards
+	# @author Diego Hernán Piccinini Lagos
+	# validation to secure the inputs of the Game
 	class ValidateGame
 		class << self
+			# Given two values String or Integer, checks whether they are valid in a context Game
+			# then return the integers values or raise an exception
+			# @param players [String|Integer]
+			# @param cards_per_player [String|Integer]
+			# @return players and cards per player [Array] with integers values of each one
 			def parse_and_validate(players, cards_per_player)
 				begin
 					players = Integer(players)
@@ -17,6 +24,10 @@ module GameShuffleCards
 				raise GameShuffleCards::TooManyCardsDemandedError if (players * cards_per_player) > GameShuffleCards::Game::TOTAL_CARDS
 				[players,cards_per_player]
 			end
+
+			# Validates whether the amount and type of players are right. Otherwise raise an exception
+			# @param players [String|Integer]
+			# @return number of players [Integer]
 			def parse_and_validate_players(players)
 				begin
 					players = Integer(players)
